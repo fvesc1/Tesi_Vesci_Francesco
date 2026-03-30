6788baa695de141014ddc3a516494da0024d6002
fvesc1 <vscfnc02e24m208p@studenti.unical.it>
2026-03-31 01:47:51 +0200

    Aggiunto ObjectiveSensor per mandare agli asp lo stato di ogni zona di controllo

---
+++ Reale tesi/Battle_of_elbing/Battle_of_elbing_Source_code/Unity-Game-Project/RTS Multiplayer/Assets/Script_IA/ObjectiveSensor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveSensor : MonoBehaviour
+{
+    [Header("Identificativo per ASP")]
+    // Questo è il nome che userà ASP per riconoscere di QUALE base stiamo parlando
+    public string objectiveID = "objective_name"; 
+
+    [Header("Dati per ASP (Sola Lettura)")]
+    // Qui scriveremo "NEUTRAL", "FRIENDLY" o "HOSTILE"
+    public string currentOwner; 
+
+    private ObjectiveInfluenceScript influenceScript;
+
+    void Start()
+    {
+        // Peschiamo lo script originale che sta sullo stesso oggetto
+        influenceScript = GetComponent<ObjectiveInfluenceScript>();
+    }
+
+    void Update()
+    {
+        // Copiamo lo stato originale in formato stringa (testo) che ASP digerisce perfettamente
+        if (influenceScript != null)
+        {
+            currentOwner = influenceScript.influenceState.ToString();
+        }
+    }
+}
