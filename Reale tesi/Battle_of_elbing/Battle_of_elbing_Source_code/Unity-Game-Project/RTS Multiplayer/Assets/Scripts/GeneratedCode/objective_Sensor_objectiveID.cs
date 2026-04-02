7b83adc1dbc42c0ef9f93b7e42498945570cb092
fvesc1 <vscfnc02e24m208p@studenti.unical.it>
2026-04-02 17:42:08 +0200

    Rimosso doppione in ogbjective, settato i sensori con objectiveSensor, che prendono id e owner per tutti gli objective

---
+++ Reale tesi/Battle_of_elbing/Battle_of_elbing_Source_code/Unity-Game-Project/RTS Multiplayer/Assets/Scripts/GeneratedCode/objective_Sensor_objectiveID.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using ThinkEngine.Mappers;
+using static ThinkEngine.Mappers.OperationContainer;
+namespace ThinkEngine
+{
+	public class objective_Sensor_objectiveID : Sensor
+	{
+		private int counter;
+		private object specificValue;
+		private Operation operation;
+		private BasicTypeMapper mapper;
+		private List<string> values = new List<string>();
+		public override void Initialize(SensorConfiguration sensorConfiguration)
+		{
+			this.gameObject = sensorConfiguration.gameObject;
+			ready = true;
+			int index = gameObject.GetInstanceID();
+			mapper = (BasicTypeMapper)MapperManager.GetMapper(typeof(string));
+			operation = mapper.OperationList()[0];
+			counter = 0;
+			mappingTemplate = "objective_Sensor_objectiveID(objective_1,objectIndex("+index+"),{0})." + Environment.NewLine;
+		}
+		public override void Destroy()
+		{
+		}
+		public override void Update()
+		{
+			if(!ready)
+			{
+				return;
+			}
+			if(!invariant || first)
+			{
+				first = false;
+				ObjectiveSensor ObjectiveSensor_1 = gameObject.GetComponent<ObjectiveSensor>();
+				if(ObjectiveSensor_1 == null)
+				{
+					values.Clear();
+					return;
+				}
+				if(ObjectiveSensor_1 == null)
+				{
+					values.Clear();
+					return;
+				}
+				string objectiveID_2 = ObjectiveSensor_1.objectiveID;
+				if(objectiveID_2 == null)
+				{
+					values.Clear();
+					return;
+				}
+				if (values.Count == 1)
+				{
+					values.RemoveAt(0);
+				}
+				values.Add(objectiveID_2);
+			}
+		}
+		public override string Map()
+		{
+			object operationResult = operation(values, specificValue, counter);
+			if(operationResult != null)
+			{
+				return string.Format(mappingTemplate, BasicTypeMapper.GetMapper(operationResult.GetType()).BasicMap(operationResult));
+			}
+			else
+			{
+				return "";
+			}
+		}
+	}
+}
