d5d56665fe884bd1af1faa9aefff4054d5b8bbd9
fvesc1 <vscfnc02e24m208p@studenti.unical.it>
2026-05-11 17:29:58 +0200

    Aggiunto sensore a ia commander che vede current money num messages in dispatcer, current unit count, max unit count e base health

---
+++ Reale tesi/Battle_of_elbing/Battle_of_elbing_Source_code/Unity-Game-Project/RTS Multiplayer/Assets/Scripts/GeneratedCode/aI_CommanderSensor_currentMoney.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using ThinkEngine.Mappers;
+using static ThinkEngine.Mappers.OperationContainer;
+namespace ThinkEngine
+{
+	public class aI_CommanderSensor_currentMoney : Sensor
+	{
+		private int counter;
+		private object specificValue;
+		private Operation operation;
+		private BasicTypeMapper mapper;
+		private List<int> values = new List<int>();
+		public override void Initialize(SensorConfiguration sensorConfiguration)
+		{
+			this.gameObject = sensorConfiguration.gameObject;
+			ready = true;
+			int index = gameObject.GetInstanceID();
+			mapper = (BasicTypeMapper)MapperManager.GetMapper(typeof(int));
+			operation = mapper.OperationList()[0];
+			counter = 0;
+			mappingTemplate = "aI_CommanderSensor_currentMoney(aI_Commander,objectIndex("+index+"),{0})." + Environment.NewLine;
+		}
+		public override void Destroy()
+		{
+		}
+		public override void Update()
+		{
+			if(!ready)
+			{
+				return;
+			}
+			if(!invariant || first)
+			{
+				first = false;
+				CommanderAI CommanderAI_1 = gameObject.GetComponent<CommanderAI>();
+				if(CommanderAI_1 == null)
+				{
+					values.Clear();
+					return;
+				}
+				if(CommanderAI_1 == null)
+				{
+					values.Clear();
+					return;
+				}
+				int currentMoney_2 = CommanderAI_1.currentMoney;
+				if (values.Count == 1)
+				{
+					values.RemoveAt(0);
+				}
+				values.Add(currentMoney_2);
+			}
+		}
+		public override string Map()
+		{
+			object operationResult = operation(values, specificValue, counter);
+			if(operationResult != null)
+			{
+				return string.Format(mappingTemplate, BasicTypeMapper.GetMapper(operationResult.GetType()).BasicMap(operationResult));
+			}
+			else
+			{
+				return "";
+			}
+		}
+	}
+}
